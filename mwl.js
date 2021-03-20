4f6767c1c60e06ccaa50124b6361ba95508efb7f
dormouse88 <dormouse88@users.noreply.github.com>
2021-03-20 23:38:10 +0000

    Added Guidance on MWL Systems

--- mwl.js
+++ mwl.js
@@ -46,19 +46,29 @@ function listif(cards, reqs)
     return writeThis;
 }
 
+function show_relevant_guidance(system)
+{
+    for (x of document.getElementsByClassName("guidance"))
+        x.style.display = "none";
+    document.getElementById(system).style.display = "block";
+}
+
 function show_chosen_mwl(mwlNum)
 {
     var writeThis = "";
     if (mwlNum in merged.data)
     {
         var chosenMWL = merged.data[mwlNum];
+        show_relevant_guidance(chosenMWL.system);
         writeThis += "<h1>" + chosenMWL.name + "</h1>";
         writeThis += "<p>Date this came into effect: " + chosenMWL.date_start + "</p>";
+        writeThis += "<p>Issuer: " + chosenMWL.issuer + "</p>";
+        writeThis += "<p>System: " + {"inf_red":"Influence Reduction", "uni_inf":"Universal Influence", "ban_and_res":"Banned and Restricted", "ban":"Ban List" }[chosenMWL.system] + "</p>";
         var sides = {"corp":"Corp","runner":"Runner"};
         for (s in sides)
         {
             writeThis += "<h2>" + sides[s] + "</h2>";
-            var limits = {"banned":"Banned", "restricted":"Restricted", "inf1":"Influence Penalty of 1", "inf3":"Influence Penalty of 3" };
+            var limits = {"banned":"Banned", "restricted":"Restricted", "inf_red":"Influence Reduction", "inf1":"Universal Influence Level 1", "inf3":"Universal Influence Level 3" };
             for (l in limits)
             {
                  var htmlList = listif(chosenMWL.cards, {"side_code":s, "limit":l});
@@ -79,12 +89,27 @@ function make_merged_data()
         for (const mwl in mwlJSON.data)
         {
             merged.data[mwl] = { "name": mwlJSON.data[mwl].name, "date_start":mwlJSON.data[mwl].date_start, "cards": {} };
+            var mwldate = new Date(mwlJSON.data[mwl].date_start);
+            if (mwldate < new Date(2018,10,1))
+                merged.data[mwl].issuer = "FFG";
+            else
+                merged.data[mwl].issuer = "NISEI";
+
+            if (mwldate < new Date(2017,1))
+                merged.data[mwl].system = "inf_red";
+            else if (mwldate < new Date(2017,6))
+                merged.data[mwl].system = "uni_inf";
+            else if (mwldate < new Date(2020,2))
+                merged.data[mwl].system = "ban_and_res";
+            else
+                merged.data[mwl].system = "ban";
+
             for (const cc in mwlJSON.data[mwl].cards)
             {
                 var mwlcard = mwlJSON.data[mwl].cards[cc];
                 var jcard = cardsJSON.data.find( function(cid){ return cid.code == cc } );
                 var limit = "";
-		if ("global_penalty" in mwlcard) limit = "inf1";
+		if ("global_penalty" in mwlcard) limit = "inf_red";
                 if ("universal_faction_cost" in mwlcard) {
                     if (mwlcard.universal_faction_cost == 1) limit = "inf1";
                     if (mwlcard.universal_faction_cost == 3) limit = "inf3";
